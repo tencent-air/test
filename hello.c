6aeae6099cbf9a50a9f10e40d9851b57d3c476c1
tencent-air <airyoureyes@aliyun.com>
2017-09-29 19:46:41 +0800

    doc: create hello.c and say hello

---
+++ hello.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2017/09/29 19:43:17
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(void){
+	printf ("hello c");
+}
